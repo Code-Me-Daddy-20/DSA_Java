44ff83ff71c4c6ee988d35dceec23abffac90f20
Sai_Binayak <sai0binayak99@gmail.com>
2024-08-16 22:08:31 +0530

    gbgfdb

--- 60_DaysOfCode/Day - 10 , 11 , 12/MediumArrays.java
+++ 60_DaysOfCode/Day - 10 , 11 , 12/MediumArrays.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class MediumArrays{
 	public static void main(String[] args) {
-		int arr[] = {3,1,-2,-5,2,-4};
+		int arr[] = {2,1,5,4,3,0,0};
 		// Arrays.sort(arr);
 		int n = arr.length;
 		// int target = 14;
@@ -14,6 +14,7 @@ public class MediumArrays{
 		// System.out.println(LeaderElementtoRight(arr, n));
 		// System.out.println(subarraywithsumK(arr, n, 4));
 		// System.out.println(Arrays.toString(rearrangeArray(arr)));
+		// System.out.println(Arrays.toString(nextPermutation(arr, n)));
 	}
 
 	// 1. Two Sum
@@ -267,6 +268,47 @@ public class MediumArrays{
 		// 	}
 		// 	return a;
 		// }
+
+		// 9. Next permutation
+		static int[] nextPermutation(int a[], int n){
+			int index = -1;
+			for (int i = n-2; i >= 0; i--) {
+				if(a[i] < a[i+1]){
+					index = i;
+					break;
+				}
+			}
+			if(index == -1){
+				int start = 0; int end = n-1;
+				while (start < end) {
+				int temp = a[start];
+				a[start] = a[end];
+				a[end] = temp;
+				start++;
+				end--;
+			}
+			return a;
+			}
+
+			for (int i = n-1; i >= index; i--) {
+				if(a[i] > a[index]){
+					int temp = a[i];
+					a[i] = a[index];
+					a[index] = temp;
+
+					break;
+				}
+			}
+			int start = index+1; int end = n-1;
+			while (start < end) {
+				int temp = a[start];
+				a[start] = a[end];
+				a[end] = temp;
+				start++;
+				end--;
+			}
+			return a;
+		}
 		
 
 }
