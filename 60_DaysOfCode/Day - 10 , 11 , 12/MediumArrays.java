459ee9404d0470ace6b4de7e518efbc7762afe40
Sai_Binayak <sai0binayak99@gmail.com>
2024-09-24 21:38:17 +0530

    d

--- 60_DaysOfCode/Day - 10 , 11 , 12/MediumArrays.java
+++ 60_DaysOfCode/Day - 10 , 11 , 12/MediumArrays.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class MediumArrays {
 	public static void main(String[] args) {
-		int arr[] = { 2, 1, 5, 4, 3, 0, 0 };
+		// int arr[] = { 2, 1, 5, 4, 3, 0, 0 };
 		// Arrays.sort(arr);
-		int n = arr.length;
+		// int n = arr.length;
 		// int target = 14;
 		// System.out.println(twoSum(arr, n, target));
 		// System.out.println(Arrays.toString(sortZerOneTwo(arr, n)));
@@ -16,6 +16,25 @@ public class MediumArrays {
 		// System.out.println(Arrays.toString(rearrangeArray(arr)));
 		// System.out.println(Arrays.toString(nextPermutation(arr, n)));
 		// System.out.println(longestConsecutive(arr));
+
+		// ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+        // matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+        // matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1)));
+        // matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+        // int n = matrix.size();
+        // int m = matrix.get(0).size();
+		// ArrayList<ArrayList<Integer>> ans1 = setZeros(matrix, n, m);
+		// ArrayList<ArrayList<Integer>> ans2 = optimalsetZeros(matrix, n, m);
+		// System.out.println("The Final matrix is: ");
+        // for (ArrayList<Integer> row : ans1) {
+        //     for (Integer ele : row) {
+        //         System.out.print(ele + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+		
 	}
 
 	// 1. Two Sum
@@ -332,6 +351,65 @@ public class MediumArrays {
     //     return lg;
     // }
 
-	
+	// 11. Set matrix 0
+	// ~ Better Approach -
+	// static ArrayList<ArrayList<Integer>> setZeros(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+	// 	int row[] = new int[n];
+	// 	int col[] = new int[m];
+	// 	for (int i = 0; i < n; i++) {
+	// 		for (int j = 0; j < m; j++) {
+	// 			if (matrix.get(i).get(j) == 0) {
+	// 				row[i] = 1;
+	// 				col[j] = 1;
+	// 			}
+	// 		}
+	// 	}
+	// 	for (int i = 0; i < n; i++) {
+	// 		for (int j = 0; j < m; j++) {
+	// 			if (row[i] == 1 || col[j] == 1)
+	// 				matrix.get(i).set(j, 0);
+	// 		}
+	// 	}
+	// 	return matrix;
+	// }
 
+	// ~ Optimal Appraoch - 
+	// static ArrayList<ArrayList<Integer>> optimalsetZeros(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+    //     int col0 = 1;
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = 0; j < m; j++) {
+    //             if (matrix.get(i).get(j) == 0) {
+    //                 // mark i-th row:
+    //                 matrix.get(i).set(0, 0);
+
+    //                 // mark j-th column:
+    //                 if (j != 0)
+    //                     matrix.get(0).set(j, 0);
+    //                 else
+    //                     col0 = 0;
+    //             }
+    //         }
+    //     }
+    //     for (int i = 1; i < n; i++) {
+    //         for (int j = 1; j < m; j++) {
+    //             if (matrix.get(i).get(j) != 0) {
+    //                 // check for col & row:
+    //                 if (matrix.get(i).get(0) == 0 || matrix.get(0).get(j) == 0) {
+    //                     matrix.get(i).set(j, 0);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     if (matrix.get(0).get(0) == 0) {
+    //         for (int j = 0; j < m; j++) {
+    //             matrix.get(0).set(j, 0);
+    //         }
+    //     }
+    //     if (col0 == 0) {
+    //         for (int i = 0; i < n; i++) {
+    //             matrix.get(i).set(0, 0);
+    //         }
+    //     }
+    //     return matrix;
+    // }
 }
