c7a15759e438aaddf359933ca557a7ab2a67044d
Sai_Binayak <sai0binayak99@gmail.com>
2024-09-24 23:37:25 +0530

    pp

--- 60_DaysOfCode/Day - 10 , 11 , 12/MediumArrays.java
+++ 60_DaysOfCode/Day - 10 , 11 , 12/MediumArrays.java
@@ -33,6 +33,18 @@ public class MediumArrays {
         //     System.out.println();
         // }
 
+		// int arr[][] =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        // rotate90(arr);
+        // System.out.println("Rotated Image");
+        // for (int i = 0; i < arr.length; i++) {
+        //     for (int j = 0; j < arr.length; j++) {
+        //         System.out.print(arr[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+		
+
 
 		
 	}
@@ -412,4 +424,25 @@ public class MediumArrays {
     //     }
     //     return matrix;
     // }
+
+	// 12. Rotate matrix by 90 degree
+	static void rotate90(int[][] matrix) {
+        for(int i = 0 ; i < matrix.length ; i++){
+            for(int j = i; j < matrix[0].length ; j++){
+                if(i!=j){
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
 }
